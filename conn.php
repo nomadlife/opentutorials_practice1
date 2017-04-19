4c5793eae8bae8eae2b674a52d07dee06067aea0
nomadlife <joonwoo3816@gmail.com>
2017-04-19 17:49:39 +0900

    2

--- conn.php
+++ conn.php
@@ -1,5 +1,6 @@
 <?php
-// 1.데이터베이스 접속
-$conn=mysqli_connect("localhost","root", "13579111");
-mysqli_select_db($conn, "opentutorials2");
-?>
+$conn = mysqli_connect('localhost','root','13579111');
+mysqli_select_db($conn, 'opentutorials2');
+$sql = "SELECT * FROM `topic`";
+$result = mysqli_query($conn,$sql);
+ ?>
